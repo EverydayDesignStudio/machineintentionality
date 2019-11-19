fcf30833c19f492db63242a27e2e19450b60196e
Navdeep Singh <navdeep.singh2088@gmail.com>
2019-11-18 21:58:23 -0800

    disable secure connection

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,7 @@ const createSendToken = (user, statusCode, res) => {
     httpOnly: true
   };
 
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  //if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
   res.cookie('jwt', token, cookieOptions);
   user.password = undefined;
